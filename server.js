65a36d40748f583bafe6874ae8832b61e06da531
IIZAANAAMII-dev <lgkyliann@gmail.com>
2025-08-06 14:18:06 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+// index.js
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur Render !');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur lancé sur le port ${PORT}`);
+});
